22b379a117781b954d8cfdeebcf20079806133a5
Skrillex <1979736774@qq.com>
2021-11-04 20:03:11 +0800

    增加model_unit

--- wechat/views.py
+++ wechat/views.py
@@ -247,6 +247,7 @@ class Model(APIView):
                     model_object_dict['scene'] = scene
                     model_object_dict['date'] = date
                     model_object_dict['vr_address'] = model_vr
+                    model_object_dict['model_unit'] = model_unit
                     model_object_dict['model_sample'] = sample_url+edition_sample+model_sample
                     model_object_dict['edition_name'] = model_object.Edition.name
                     model_object_dict['edition_style'] = model_object.Edition.style
